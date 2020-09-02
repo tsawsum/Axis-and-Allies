0799f9118176f2ac20ae8b12159b971369931aa4
tsawsum <johnsong@mssm.org>
2020-09-02 10:43:04 -0700

    Update phases.py

--- phases.py
+++ phases.py
@@ -4,20 +4,74 @@ import math
 
 class Build:
     def __init__(self, game): # game.turn_state, etc.
+        self.game = game
+        self.ipc = self.game.turn_state.player.ipc
+
+        self.prioritize_surface_navy = 0
+        self.prioritize_transports = 0  #this should also purchase transportable units if needed
+        self.prioritize_subs = 0
+        self.prioritize_air = 0
+        self.prioritize_land_defense = 0
+        self.prioritize_land_creeping_offense = 0
+        self.prioritize_land_blitz = 0
+
+        self.factories = {}
+        for territory_key in self.game.state_dict:
+            for unit_state in self.game.state_dict[territory_key].unit_state_list:
+                if unit_state.type_index == 5:
+                    self.factories[territory_key] = self.game.rules.board[territory_key].ipc #name keys to ipc value
+        # factories now has a dict of names having factories and their IPC values (max build capacity)
+
+        self.immediate_defensive_requirements = []
+        self.latent_defensive_requirements = []
+
+    def build_strategy(self):  #called by the AI
+        #TODO 1. make sure your defensive requirements are met if possible. If land prioritization is active
+        #           utilize specific units if possible. Set low prioritiy if you want to build only tanks in SA
+        #     2. If not possible with land prioritization, replace special units with infantry.
+        #     3. If still not possible, buy the max amount of infantry and prioritize based on territory ipc value
+        #     4. If sea territory must be held, repeat above process with carriers as fallback defense if planes in
+        #           range, otherwise cruisers.
+        #     5. Order active prioritizations from highest to lowest
+        #     6. Allocate IPC according to the ratio of prioritizations. For example, prioritize_subs = 2
+        #           prioritize_air = 1, try to build about twice as much value in subs as air. Generally,
+        #           certain countries will have a slight value (say 1) for long term needed builds like extra infantry
+        #     6.5. Check if more units than build slots. If so, dont build all of the above step.
+        #     7. Check remaining IPC and buy extra units if space. If not, upgrade units as long as they were not purchased
+        #           due to the transports prioritization (bc then they may not fit)
+
+        # This whole time, these purchased units should be added to a theortical dict called "placements" (name to unit list)
         pass
 
-    def build_unit(self, territory, unit_index): #called by the AI
-        pass
+    def prioritizer(self, prioritization, strength):
+        self.prioritization = strength
+
+    #TODO Utilize combat move attack decision module to determine which factories/ key neighbors are under threat
+    # This will need to be called within build_strategy after adding the theoritical units to see if still under threat.
+    def set_defensive_requirements(self, endangered_name_list):
+        for territory_name in endangered_name_list:
+            if (territory_name in factories):
+                self.immediate_defensive_requirements.append(territory_name)
+
+            for territory_key in factories:
+                if territory_name == self.game.rules.connections[territory_key]:
+                    self.latent_defensive_requirements.append(territory_key)
+
+    def build_unit(self, territory_name, unit_index): #called by the AI
+        self.game.state_dict[territory_name].append(self.game.rules.get_unit(unit_index))
+        self.ipc = self.ipc - self.game.rules.get_unit(unit_index).cost
 
 
 class Combat_Move:
-    pass
+    def __init__(self, game):
+        pass
 
     def battle_calculator(self):
     #MAKE A COPY OF STUFF BEFORE YOU USE THIS BC THE BATTLE STUFF ACTUALLY MAKES CHANGES
     #account for land units on transpots in ipc swing
     # tool for the AI to determine how a battle WOULD turn out without actually changing the GameState
         pass
+
 
 class Battles:
     def __init__(self, game):
@@ -92,7 +146,8 @@ class Battles:
         friendly_units = []
 
         for unit_state in unit_state_list:
-            if self.game.rules.teams[unit_state.owner] == self.team:
+            if (self.game.rules.teams[unit_state.owner] == self.team) \
+            and (self.game.rules.units[unit_state.type_index].movement == 0):  #removes factories from consideration
                 friendly_units.append(unit_state)
 
         #check if can simply wipe the list
@@ -102,6 +157,7 @@ class Battles:
 
         #saving list keeps caviats if set to True
         saving_list = []
+
         if one_land_unit_remaining:
             highest_cost = 0
             for unit_state in friendly_units:
@@ -114,7 +170,7 @@ class Battles:
                     saving_list.append(unit_state)
                     friendly_units.remove(unit_state)
 
-        #will likely be either planes for defensive retreat, subs for submerge ability, or bombers on defense
+        #will likely be either planes for defensive retreat, subs for submerge ability, or bombers on defense. or carriers
         if prioritize_unit_index != -1:
             for unit_state in friendly_units:
                 if unit_state.type_index == prioritize_unit_index:
@@ -124,7 +180,7 @@ class Battles:
         #TODO CHECK IF THIS GOES INFINITE
         while casualty_count > 0:
 
-            #choose based on power
+            #choose based on power. This will automatically choose AA guns first
             lowest_power = 6
             for unit_state in friendly_units:
                 if self.game.rules.units[unit_state.type_index].attack_or_defense <= lowest_power:
@@ -151,21 +207,30 @@ class Battles:
                         lowest_power_list.remove(unit_state)  # for the while loop
                         casualty_count = casualty_count - 1
 
+            # refills friendly_units with the units we tried to save if there are too many deaths
             if friendly_units == []:
+                for unit_state in friendly_units:
+                    friendly_units.append(unit_state)
+                    saving_list.remove(unit_state)
+
+            if (friendly_units == []) and (saving_list == []):
                 break
-
-
-
-
-
-
-
 
 class Non_Combat:
     pass
 
 class Place:
-    pass
+    def __init__(self, game, placements):
+        self.placements = placements
+        self.game = game
+
+    def place(self):
+        for territory_name in self.placements:
+            self.game.state_dict[territory_name].unit_state_list.append(placements[territory_name])
 
 class Cleanup:
-    pass
+    def __init__(self, game):
+        self.game = game
+
+    # reset factory ownership
+    # murder planes. collect money. untap ships. reset movement.
