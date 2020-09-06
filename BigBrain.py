b9ba87e33d111ad678a45a183e00d96a627521dc
tsawsum <johnsong@mssm.org>
2020-09-06 15:33:19 -0700

    Create BigBrain.py

---
+++ BigBrain.py
@@ -0,0 +1,12 @@
+"""
+Some Heuristics to consider:
+ - Total Unit Value Ratio (Axis vs Allies) compared to the start to determine who is winning
+ - Total income compared to start
+ - Number of 'combat important territories' you control / have ranged jurisdiction over
+ - Number of your vs. their factories that are in danger
+ - Create some sort of "Similarity distance' function to compare current board with known 'winning' boards
+ - Overall 'luck' value. Do battle calculator before every battle and add up the difference between expected ipc_swing for your side and actual
+ - Could do a similar luck value based on percentages, where an actual win is 1 and a loss 0.
+ - 
+ 
+"""
