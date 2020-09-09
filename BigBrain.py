f390b56e8df394bbb1393bc037514bc74d6032ec
tsawsum <johnsong@mssm.org>
2020-09-09 15:44:14 -0700

    Update BigBrain.py

--- BigBrain.py
+++ BigBrain.py
@@ -15,5 +15,25 @@ Some Heuristics to consider:
  - Obviously number of controlled capitals.
  - Number of factories taken from enemy
  - which team controls suez
- 
+
+List of things AI needs to do:
+- Needs to decide if we are winning
+ * use above stuff
+- Needs to decide risk tolerance
+ * Used in is_vulnerable and everywhere that it is used.
+ * What is the minimum threshold ratio of the Hawkes formula that the AI will tolerate
+ * < 1 - risk tolerance is vulnerable
+ * Look at past battles and decide if they won in the end and what the 'risk tolerance' was
+ * I have a higher risk tolerance if I am losing
+- Needs to make an 'importance' value of each territory:
+ * Informed by webcrawled games.
+- Factory builder
+ * each territory from other games has a average factory value (how many were there and whose).
+ * if you are the player, you will build a factory there according to those values and if you can hold it
+ * Make sure we impliment both purchase and placement (place the factory at the end so it doesnt think it can build in it)
+- Calculate build average for each teritory
+ * build importance determine max build in unthreateneed territories
+- AI organize placements list
+ * WHAT THE FUCK WERE WE THINKING WHYYYYYYY
+- Needs to decide build priority values.
 """
